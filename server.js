d42a267ad23395e2cdb45f335aa38f6b47272233
jackhart591 <jackhart591@gmail.com>
2022-05-26 11:01:59 -0700

    Worked on CSS
    I finished aligning the items and started applying a color palette. I
    think the colors still needs work, and I need to make my CSS better
    apply over a general site and not just this specific page.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
 app.use(express.static('public'));
 
