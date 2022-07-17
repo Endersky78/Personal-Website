c33b0f22b3e973ff1a2ba66570875f4bb1bcacff
Endersky78 <jackhart591@gmail.com>
2022-07-16 17:18:12 -0700

    Templatized my page and started on the experience page

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-var path = require('path');
-var express = require('express');
+var path = require('path')
+var express = require('express')
+var exphbs = require('express-handlebars')
 
-var app = express();
-var port = process.env.PORT || 8000;
+var app = express()
 
-app.use(express.static('public'));
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+var port = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+  res.status(200).render('index')
+})
+
+app.get('/experience', (req, res) => {
+  res.status(200).render('experience')
+})
+
+app.use(express.static('public'))
 
 app.get('*', function (req, res) {
   //res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 app.listen(port, function () {
-  console.log("== Server is listening on port", port);
+  console.log("== Server is listening on port", port)
 });
