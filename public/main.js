0cdb4a64dd293fc5d5e74200711101c40512fb18
Endersky78 <jackhart591@gmail.com>
2022-09-03 17:07:33 -0700

    Finished first version of website

--- public/main.js
+++ public/main.js
@@ -8,5 +8,4 @@ if (url.includes('projects')) { navitems[1].classList.add('navlink-active') }
 else if (url.includes('experience')) { navitems[2].classList.add('navlink-active') }
 else if (url.includes('blog')) { navitems[3].classList.add('navlink-active') }
 else if (url.includes('resume')) { navitems[4].classList.add('navlink-active') }
-else if (url.includes('contact')) { navitems[5].classList.add('navlink-active') }
 else { navitems[0].classList.add('navlink-active') }
